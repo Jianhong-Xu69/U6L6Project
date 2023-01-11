2459486a173fd162430ae52dbe5fd49fba85d815
unknown <BT_4N2_14@BKTECH.LOCAL>
2023-01-11 12:17:52 -0500

    Patchnotes v1.00
    Archived practice work with arrays
    Imported ArrayAlgorithms.java
    Added new methods

---
+++ src/FrontDoor.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class FrontDoor {
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 3, 4, 7};
+        int[] shiftedLeftArr = ArrayAlgorithms.shiftLeft(nums);
+        System.out.println(Arrays.toString(shiftedLeftArr));
+
+        // original array NOT modified
+        System.out.println(Arrays.toString(nums));
+    }
+}
